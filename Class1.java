f07e0d23596a888fc59a7f0d6f57ce9102673455
lokeshselvam <30903712+lokeshselvam@users.noreply.github.com>
2021-08-28 15:31:13 +0530

    Add files via upload

---
+++ Class1.java
@@ -0,0 +1,12 @@
+package session1;
+
+public class Class1 {
+	public static void main(String[] args) {
+		String name="Lokesh";
+		int age = 19;
+		printName(name);
+	}
+	public void printName(String str) {
+		System.out.println(str);
+	}
+}
